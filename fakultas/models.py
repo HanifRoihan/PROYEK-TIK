5affb4a4cef43a04d33ed8efb05771ba11ecfd79
HanifRoihan <rohanstar2019@gmail.com>
2022-10-06 14:03:06 +0700

    membuat database

--- fakultas/models.py
+++ fakultas/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Dosen(models.Model):
+    NIPDosen= models.CharField(max_length=30)
+    NAMA = models.CharField(max_length=30)
+    TTL = models.CharField(max_length=30)
+    EMAIL = models.CharField(max_length=30)
+    FAKULTAS = models.CharField(max_length=30)
+    PRODI = models.CharField(max_length=30)
+    ALAMAT = models.CharField(max_length=30)
+    PHOTO = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.NIPDosen
+
+class Tendik(models.Model):
+    NIPTendik= models.CharField(max_length=30)
+    NAMA = models.CharField(max_length=30)
+    TTL = models.CharField(max_length=30)
+    EMAIL = models.CharField(max_length=30)
+    FAKULTAS = models.CharField(max_length=30)
+    PRODI = models.CharField(max_length=30)
+    ALAMAT = models.CharField(max_length=30)
+    PHOTO = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.NIPTendik
+
+class Mahasiswa(models.Model):
+    NIK= models.CharField(max_length=30)
+    NAMA = models.CharField(max_length=30)
+    TTL = models.CharField(max_length=30)
+    EMAIL = models.CharField(max_length=30)
+    FAKULTAS = models.CharField(max_length=30)
+    PRODI = models.CharField(max_length=30)
+    ALAMAT = models.CharField(max_length=30)
+    PHOTO = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.NIK
